f8bc53c3319f6c7260dc455d443e6a40546a809f
Paulo Afonso da Cruz <paulo.cruz20@fatec.sp.gov.br>
2023-07-28 15:55:47 -0300

    alterando pastas de trabalho

--- scripts.js
+++ scripts.js
@@ -2,17 +2,17 @@ let key = "4827f6b2dfb96f9cd4fb364d21442025"
 
 function colocarDadosTela(dados) {
     document.querySelector(".city").innerHTML="Tempo em" + dados.name
-    document.querySelector(".temp").innerHTML= Math.floor(dados.main.temp)
+    document.querySelector(".temp").innerHTML= Math.floor(dados.main.temp)+"°C"
     document.querySelector(".txt-prev").innerHTML=dados.whater[0].description
-    document.querySelector(".umidade").innerHTML=dados.main.humidity
+    document.querySelector(".umidade").innerHTML=dados.main.humidity + "%"
+    document.querySelector(".img-temp").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
 }
 
-async function searchCity(){
-    let dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(resposta => resposta.json{})
-    
-}
+async function buscarCidade(cidade){
+    let dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}}&appid=${key}`).then(resposta => resposta.json()) }
 
-function clickOfButton() {
-   let cidade = document.querySelector(".input-cidade").value
-    searchCity(cidade)
+function cliqueiNoBotao(){
+    let cidade = document.querySelector(".input-cidade").value
+     
+    buscarCidade(cidade)
 }
